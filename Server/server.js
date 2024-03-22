c73bff32c40fce716db2f204d01e22b04d9b9021
Harshvardhansingh <harshitharsha778@gmail.com>
2024-03-22 10:28:33 +0530

    newcommit

--- Server/server.js
+++ Server/server.js
@@ -6,26 +6,12 @@ app.use(express.json());
 const userRoute = require("./routes/userRoute");
 const adminRoute = require("./routes/adminRoute");
 const doctorRoute = require("./routes/doctorsRoute");
-const path = require("path");
+// const path = require("path");
 
 app.use("/api/user", userRoute);
 app.use("/api/admin", adminRoute);
 app.use("/api/doctor", doctorRoute);
 
-// // if (process.env.NODE_ENV === "production") {
-// //   app.use("/", express.static("client/build"));
-
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "client/build/index.html"));
-//   });
-// // }
-// static files 
-app.use(express.static(path.join(__dirname,"./client/build")));
-
-app.get("*",(req,res)=>{
-  res.sendFile(path.join(__dirname,"./client/build/index.html"));
-
-})
 const port = process.env.PORT || 5000;
 
 
